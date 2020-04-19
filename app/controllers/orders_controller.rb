05ec2dcb370890f93655fe50df08fb2ebbc1eb9a
eslam <eslamtaher333@yahoo.com>
2020-04-20 01:49:33 +0200

    add orders

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,43 @@
+class OrdersController < ApplicationController
+    def index
+        @orders = Order.all
+    end
+    def create
+        @order = Order.new(order_params)
+        if @order.save
+            redirect_to @order
+        else
+            render 'new'
+        end
+    end
+    def show
+        @order = Order.find(params[:id])
+    end
+    def new
+        @order = Order.new
+
+        def upload
+            uploaded_file = params[:picture]
+            File.open(Rails.root.join('public', 'uploads', uploaded_file.original_filename), 'wb') do |file|
+              file.write(uploaded_file.read)
+            end
+          end
+    end
+    def edit
+
+    end
+    def update
+
+    end
+    def destroy
+
+    end
+
+ 
+
+  private
+  def order_params
+   
+    params.require(:order).permit(:order_for, :resturant_name, :firend, :picture)
+  end
+end

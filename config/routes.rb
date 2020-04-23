f47f047fd814073fa5f8f5bb576f001bf29cf4dd
admin <admin@localhost.localdomain>
2020-04-23 14:19:45 +0200

    Orders Views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'orders/index'
     resources :homes
     resources :groups
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
     root 'homes#index'
+    resources :orders
 end

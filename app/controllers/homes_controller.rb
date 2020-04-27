0d680b8382fe0743de9b932bac00f36725180beb
Mohamed Elnagar <mohamedelnagar461@yahoo.com>
2020-04-27 22:42:14 +0200

    home mohamed

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,23 +1,23 @@
 class HomesController < ApplicationController
-    def index
+  def index
+    @order = Order.order(created_at: :desc).last(5)
+  end
+  def create
 
-    end
-    def create
+  end
+  def show
 
-    end
-    def show
+  end
+  def new
 
-    end
-    def new
+  end
+  def edit
 
-    end
-    def edit
+  end
+  def update
 
-    end
-    def update
+  end
+  def destroy
 
-    end
-    def destroy
-
-    end
+  end
 end
